ac1d7531080860c6c5d0b051d33141dc624a387b
Archangelius <kevin.corbett1@marist.edu>
2015-09-20 21:55:30 -0400

    Working on an idea

--- Project 1/Project1.js
+++ Project 1/Project1.js
@@ -78,6 +78,29 @@ function mkStrip() {
     var height, i; // best practice in JS is to declare our variables up front
     var points = []; // to hold the individual coordinate triples
     var vertices = []; // to hold the vertices to be drawn as tri-strips
+    var triangleStrip = [];
+    var cols = 4, rows = 3;
+
+    var numVertGrid = 2 * cols * (rows - 1);
+    var numVertStrip = 2 * cols * (rows - 1) + 2 * (rows - 2);
+    numVertices = numVertStrip;
+    var j = 0;
+    var y = 1;
+    for (var i = 1; i <= numVertGrid; i += 2) {
+        height = Math.random();
+        triangleStrip.push(vec3(((1 + i) / 2), y, height));
+        triangleStrip.push(vec3(((cols * 2 + i + 1) / 2), y-0.4, height));
+        if (triangleStrip[j + 1] % cols == 0) {//wont be able to do anything because datatype is vec3 not a number mv.js might have something
+            if (triangleStrip[j + 1] != cols && triangleStrip[j + 1] != cols * rows) {
+                triangleStrip.push(triangleStrip[j + 1]);
+                triangleStrip.push(vec3(((1 + i + 2) / 2), y-0.8, height));
+                j += 2;
+            }
+        }
+        y-=0.4
+        j += 2;
+    }
+
 
     // generate a thin 2x10 grid of points with random heights
         for (i = 0; i < 11; i++) {
@@ -88,12 +111,9 @@ function mkStrip() {
             height = Math.random();
             points.push(vec3(-1.0 + i * 0.2, -.2, height));
         }
-        for (i = 0; i < 11; i++) {
-            height = Math.random();
-            points.push(vec3(-1.0 + i * 0.2, -.6, height));
-        }
+
     // fill up the vertices array with the necessary points
-    for (i = 0; i < 22; i++) {
+    for (i = 0; i < 11; i++) {
         vertices.push(points[i], points[i + 11]);
     }
     return vertices;
