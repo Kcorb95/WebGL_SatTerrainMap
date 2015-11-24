bca50d1008cd0db2b196234c1ac2ba31b07811d9
Archangelius <kevin.corbett1@marist.edu>
2015-11-24 14:19:55 -0500

    Fixing lighting, nothing renders

--- Project Files/Project.js
+++ Project Files/Project.js
@@ -252,6 +252,8 @@ function makeStrip() {
             indices.push(i + nrows, i + 1);
         }
     }
+    console.log("normals[1][1]: " + normals[1][1]);
+    console.log("normals[ncols-2][nrows-2]: " + normals[DEMObj.ncols - 2][DEMObj.nrows - 2]);
 
     return {vertices: vertices, indices: indices, normals: normals};
 }
